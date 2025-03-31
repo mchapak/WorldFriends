7655cb1f4150faac42a908ff8845470b2fa10b5d
Moses Chapa Kiti <mchapak@gmail.com>
2025-04-01 01:34:20 +0300

    malaria subcounty trend function

---
+++ src/functions.R
@@ -0,0 +1,49 @@
+# Plot the total number of malaria cases by county
+
+plot_total_malaria_cases <- function(data, title_text, file_name) {
+  plot <- ggplot(data, aes(x = my, y = total, 
+                           group = case_type, 
+                           color = case_type, 
+                           fill = case_type)) + 
+    
+    # Bar plot for suspected cases
+    geom_col(data = data %>% filter(case_type == "Suspected",
+    ), 
+    aes(fill = case_type), 
+    width = 1, alpha = 0.4) +  # Transparency to differentiate bars
+    
+    # Line plot for tested and confirmed cases
+    geom_line(data = data %>% filter(case_type != "Suspected"), 
+              aes(color = case_type), 
+              size = 1.2) + 
+    
+    # Points for tested and confirmed cases
+    geom_point(data = data %>% filter(case_type != "Suspected"), 
+               aes(color = case_type), 
+               size = 2) + 
+    
+    # Labels and theme
+    labs(title = "", # paste("Monthly", title_text, "malaria cases"),
+         x = "Month-Year of reporting",
+         y = "Number of Cases",
+         color = "Case Type",
+         fill = "Case Type") + 
+    
+    # Ensure both color and fill use the same scale
+    scale_color_manual(values = c(col_case[1], col_case[2], col_case[3])) +  
+    scale_fill_manual(values = c(col_case[1], col_case[2], col_case[3])) +  
+    
+    facet_wrap(~ subcounty, ncol = 1) +  
+    theme_minimal() +
+    
+    # Improve readability of the plot
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 14),
+          axis.text.y = element_text(size = 18),
+          axis.title = element_text(size = 20),
+          title = element_text(size = 20),
+          strip.text = element_text(size = 18, face = "bold"),
+          legend.text = element_text(size = 18),
+          legend.title = element_text(size = 20, face = "bold"))
+  
+  return(plot)
+}
