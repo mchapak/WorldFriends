c1df15307a42ffd3822a830e7998edcf5752019d
Moses Chapa Kiti <mchapak@gmail.com>
2025-07-14 21:02:02 +0300

    updated mip and positicvity comparison

--- src/functions.R
+++ src/functions.R
@@ -4,14 +4,9 @@
 
 
 # SET GLOBAL VALUES
-# Ganze, Kaloleni, Kilifi North, Kilifi South, Malindi, Magarini, Rabai
-map_cols <- c('#8dd3c7', '#d8b365', '#ef8a62', '#bebada', '#bdbdbd', '#bdbdbd', '#b3de69')
+map_cols <- c('#8dd3c7', '#ef8a62', '#bebada', '#d8b365', '#b3de69')
 disp_cols <- c('#8dd3c7', '#ef8a62')
-col_case <- c("#bdbdbd", "#7570b3", "#d95f02") #  "#542788"
-bg_col <- "white"
-text_hil <- "grey30"
 
-#-------------------------------------------------------------------------------#
 
 # create functions to transform data
 # a. write functions to reshape data into correct format
@@ -33,11 +28,14 @@ reshape_moh705 <- function(data) {
            my = paste0(month,"-",year))
 }
 
-#-------------------------------------------------------------------------------#
+
 # b. define report month levels
 report_month <- c("9-23","10-23","11-23","12-23","1-24","2-24","3-24","4-24",
                   "5-24","6-24","7-24","8-24","9-24","10-24","11-24","12-24",
                   "1-25","2-25","3-25","4-25","5-25")
+
+col_case <- c("#bdbdbd", "#7570b3", "#d95f02") #  "#542788"
+
 
 # create function to plot malaria cases, overall
 plot_malaria_cases <- function(data, title_text, file_name) {
@@ -64,7 +62,7 @@ plot_malaria_cases <- function(data, title_text, file_name) {
     # Labels and theme
     labs(title = "", # paste("Monthly", title_text, "malaria cases"),
          x = "Month-Year of reporting",
-         y = "Malaria",
+         y = "Number of Cases",
          color = "Case Type",
          fill = "Case Type") + 
     
@@ -94,7 +92,7 @@ save_plot <- function(plot, file_name){
          height = 6, width = 15, dpi = 300, bg = "#FFFFFF")
 }
 
-#-------------------------------------------------------------------------------#
+
 # Plot the total number of malaria cases by county
 plot_total_malaria_cases <- function(data, title_text, file_name) {
   plot <- ggplot(data, aes(x = my, y = total, 
@@ -121,7 +119,7 @@ plot_total_malaria_cases <- function(data, title_text, file_name) {
     # Labels and theme
     labs(title = "", # paste("Monthly", title_text, "malaria cases"),
          x = "Month-Year of reporting",
-         y = "Malaria",
+         y = "Number of Cases",
          color = "Case Type",
          fill = "Case Type") + 
     
@@ -144,7 +142,7 @@ plot_total_malaria_cases <- function(data, title_text, file_name) {
   return(plot)
 }
 
-#-------------------------------------------------------------------------------#
+
 # # Plot the number of people with nets and those using
 # plot_net_usage <- function(data, value, title){
 #   plot <- ggplot(data, 
@@ -170,50 +168,45 @@ plot_total_malaria_cases <- function(data, title_text, file_name) {
 #   return(plot)
 # }
 
-#-------------------------------------------------------------------------------#
+
 # GRAPH: OVERALL NET USAGE AT THE SUBCOUNTY LEVEL
 fxn_fig_net_subcounty <- function(data, x_var, x_axis_label){
   ggplot(data, 
-         aes(x = !!sym(x_var), 
-             y = reorder(sub_county, !!sym(x_var)))) +  # Removed fill from aes()
+         aes(x = !!sym(x_var), # use the variable whose name is stored inside x_var
+             y = reorder(sub_county, !!sym(x_var)),
+             fill = sub_county)) +
     
-    # Use fill scale manually
-    geom_col(fill = map_cols[c(1:4,7)]) +  # Fill applied here, not in aes()
+    geom_col(fill = map_cols) +
     
-    # Text labels 
-    geom_text(aes(label = paste0(!!sym(x_var), "%")),
-              hjust = 1.1, 
-              size = 7, 
-              color = "white",  # This will now be applied correctly
-              fontface = "bold") +
+    geom_text(aes(label = paste0(paste0(!!sym(x_var), "%"), 
+                                 " (n=", n_sensitized, ")")),  # Display proportion as percentage
+              hjust = -0.1, size = 7, color = "black", fontface = "bold") +  # Adjust text position
     
-    labs(x = x_axis_label,
-         y = "Sub-County") +
+    labs(#title = "Proportion of people with nets\n out of those sensitized",
+      x = x_axis_label,
+      y = "Sub-County") +
     
-    scale_x_continuous(labels = function(x) paste0(x, "%"), 
-                       limits = c(0, 100)) +
+    scale_x_continuous(labels = function(x) paste0(x, "%"), limits = c(0, 100)) +
     
     theme_minimal() +
-    theme(axis.text = element_text(size = 18),
-          axis.title = element_text(size = 25, face = "bold"),
-          legend.position = "none")
+    
+    theme(axis.text = element_text(size = 16),
+          axis.title = element_text(size = 25, face = "bold"))
 }
 
-#-------------------------------------------------------------------------------#
+
 # GRAPH: NET USAGE AT THE HEALTH FACILITY LEVEL
 fxn_fig_net_dispensary <- function(data, subcounty, x_var, x_axis_label){
   ggplot(data %>% filter(sub_county == subcounty), 
          aes(x = !!sym(x_var), # use the variable whose name is stored inside x_var
-             y = reorder(dispensary, !!sym(x_var)))) +
+             y = reorder(dispensary, !!sym(x_var)),
+             fill = dispensary)) +
     
     geom_col(fill = map_cols[1:2]) +
     
-    # Text labels 
-    geom_text(aes(label = paste0(!!sym(x_var), "%")),
-              hjust = 1.1, 
-              size = 7, 
-              color = "white",
-              fontface = "bold") +
+    geom_text(aes(label = paste0(paste0(!!sym(x_var), "%"), 
+                                 " (n=", n_sensitized, ")")),  # Display proportion as percentage
+              hjust = -0.1, size = 7, color = "black", fontface = "bold") +  # Adjust text position
     
     labs(#title = "Proportion of people with nets\n out of those sensitized",
       x = x_axis_label,
@@ -223,12 +216,12 @@ fxn_fig_net_dispensary <- function(data, subcounty, x_var, x_axis_label){
     
     theme_minimal() +
     
-    theme(axis.text = element_text(size = 18),
-          axis.title = element_text(size = 25, face = "bold"),
-          legend.position = "none")
+    theme(axis.text = element_text(size = 16),
+          axis.title = element_text(size = 25, face = "bold"))
 } 
 
-#-------------------------------------------------------------------------------#
+
+
 # GRAPH: MALARIA TESTS AND POSITIVITY
 plot_malaria_test_type <- function(data, test_type, test_name, age, facet_var){
   ggplot(data,
